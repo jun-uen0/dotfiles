b02a79f63c41426cd9858c8352d26ea42652e792
dobashi <dobashi@lavans.jp>
2022-01-24 12:24:36 +0900

    commet out npm

--- setup.d/npm.sh
+++ setup.d/npm.sh
@@ -1,6 +1,10 @@
 #/bin/sh
 
-sudo apt install -y npm nodejs
+# @todo nvm install
+
+
+# sudo apt install -y npm nodejs
+
 #npm i -D webpack webpack-cli typescript ts-loader
 #npm i -S three @types/three
 
